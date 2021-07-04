7589d5489e31f3af7d5e11f625bdb453c7eb0807
Omer Gery <68545675+OmerGery@users.noreply.github.com>
2021-07-04 15:02:58 +0300

    Update lamda.py

--- lamda.py
+++ lamda.py
@@ -1,5 +1,6 @@
 import requests
-
+import json
+STOCKS_AMOUNT = 5
 
 def getStockData():
     url = "https://latest-stock-price.p.rapidapi.com/price"
@@ -11,9 +12,22 @@ def getStockData():
         'x-rapidapi-host': "latest-stock-price.p.rapidapi.com"
     }
 
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    return response.text
+    all_stocks = json.loads(requests.request("GET", url, headers=headers, params=querystring).text)
+    result_top5 = ""
+    i = 0
+    for stock in all_stocks:
+        if i==0:
+            i += 1
+            continue
+        if i==STOCKS_AMOUNT+1:
+            break
+        result_top5 += stock["identifier"] + " " + str(stock["pChange"]) + "\n"
+        # result_top5 += " pChange:"
+        # precent_up = stock["pChange"]
+        # result_top5 += str(4)
+        i += 1
 
+    return result_top5
 
 def lambda_handler(event, context):
     string = getStockData()
